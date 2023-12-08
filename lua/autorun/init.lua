36c7aeb03af86e9a9da63ae2c452a80597e39307
YoItSoul <scalfua@gmail.com>
2023-12-08 16:18:13 -0600

    Cleaning up original features and adding modular compatibility for other gamemodes.

--- lua/autorun/init.lua
+++ lua/autorun/init.lua
@@ -1,12 +1,29 @@
---include("addons/conquest_leveling/modules/base/your_file.lua")
+local _, folders = file.Find("modules/*", "LUA")
+for _, folder in pairs(folders) do
+    local path = "modules/" .. folder .. "/"
+    local serverFiles = file.Find(path .. "sv_*.lua", "LUA")
+    local sharedFiles = file.Find(path .. "sh_*.lua", "LUA")
+    local clientFiles = file.Find(path .. "cl_*.lua", "LUA")
+    if SERVER then
+        for _, svFile in pairs(serverFiles) do
+            include(path .. svFile)
+        end
 
-if SERVER then
-    include("modules/base/conquest_config.lua")
-    include("modules/base/sv_conquest.lua")
-    AddCSLuaFile("modules/base/conquest_config.lua")
-    AddCSLuaFile("modules/base/cl_conquest.lua")
-else
-    include("modules/base/conquest_config.lua")
-    include("modules/base/cl_conquest.lua")
+        for _, shFile in pairs(sharedFiles) do
+            include(path .. shFile)
+            AddCSLuaFile(path .. shFile)
+        end
+
+        for _, clFile in pairs(clientFiles) do
+            AddCSLuaFile(path .. clFile)
+        end
+    else
+        for _, shFile in pairs(sharedFiles) do
+            include(path .. shFile)
+        end
+
+        for _, clFile in pairs(clientFiles) do
+            include(path .. clFile)
+        end
+    end
 end
-
